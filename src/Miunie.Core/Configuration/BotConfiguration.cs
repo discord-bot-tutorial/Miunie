11b55391653da60ba5f1961e1b24804d317e22be
petrspelos <petrspelos@users.noreply.github.com>
2020-02-10 23:58:23 +0100

    Add License to BotConfiguration

--- src/Miunie.Core/Configuration/BotConfiguration.cs
+++ src/Miunie.Core/Configuration/BotConfiguration.cs
@@ -1,4 +1,10 @@
-﻿namespace Miunie.Core.Configuration
+﻿// Copyright (C) 2020  Control.NET <https://github.com/control-net>
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+namespace Miunie.Core.Configuration
 {
     public class BotConfiguration : IBotConfiguration
     {
